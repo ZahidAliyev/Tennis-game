443da48054914c8b46b8a0dc5f8978dbeaa8dc18
Zahid Aliyev <hermitaliyev96@gmail.com>
2020-03-12 16:41:07 +0400

    first menu

--- eventFunctions.js
+++ eventFunctions.js
@@ -1,15 +1,23 @@
 //CLICK TO CONTINUE
 
 function clickToContinue(e) {
-
   if (showWinScreen) {
-    playerScore = 0;
-    compScore = 0;
-    showWinScreen = false;
-    ballXspeed = 5;
-    ballYspeed = 5;
+    if (
+      e.offsetX >= 340 &&
+      e.offsetX <= 500 &&
+      e.offsetY >= 230 &&
+      e.offsetY <= 275
+    ) {
+      playerScore = 0;
+      compScore = 0;
+      showWinScreen = false;
+      ballXspeed = 5;
+      ballYspeed = 5;
+    }
   }
 }
+//BACK TO MENU AFTER GAME LEVEL FINISHED
+function backToMenu(e) {}
 //PLAYER PADDLE MOVE
 function handleMouse(e) {
   var rect = canvas.getBoundingClientRect();
@@ -19,15 +27,15 @@ function handleMouse(e) {
 }
 // START 1 PLAYER GAME
 function startOnePlayer(e) {
+  console.log(e.offsetX,e.offsetY);
   if (
     e.offsetX >= 350 &&
     e.offsetX <= 430 &&
     e.offsetY >= 280 &&
     e.offsetY <= 310
   ) {
-    
-    console.log("startOnePlayer -> startGame", startGame)
-    if(startGame) {
+    console.log("startOnePlayer -> startGame", startGame);
+    if (startGame) {
       function loop() {
         move();
         someSound.play();
@@ -39,6 +47,6 @@ function startOnePlayer(e) {
       console.log("start");
     }
     startGame = false;
-    console.log("startOnePlayer -> startGame", startGame)
+    console.log("startOnePlayer -> startGame", startGame);
   }
 }

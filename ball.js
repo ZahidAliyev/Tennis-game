2aa363f7b1982617a5b8dac6b36bd5e9c9bef59c
Zahid Aliyev <hermitaliyev96@gmail.com>
2020-03-13 20:10:11 +0400

    paddle collision problems

--- ball.js
+++ ball.js
@@ -1,9 +1,9 @@
-//Ball parametrs
+                          //---------Ball parametrs
 var ballX = canvasWidth / 2;
 var ballY = canvasHeight / 2;
 var ballRadius = 10;
 
-//beginning ball random directions
+                            //beginning ball random directions
 var xDirection = Math.random() < 0.5 ? 1 : -1;
 var ballXspeed = 5;
 ballXspeed = ballXspeed * xDirection;
@@ -11,7 +11,7 @@ ballXspeed = ballXspeed * xDirection;
 var yDirection = Math.random() < 0.5 ? 1 : -1;
 var ballYspeed = 5;
 ballYspeed = ballYspeed * yDirection;
-
+                          //--------- Ball movement after goal
 function resetBall() {
   if (playerScore >= winningScore || compScore >= winningScore) {
     showWinScreen = true;
@@ -25,24 +25,33 @@ function resetBall() {
   let plusOrminus = Math.random() < 0.5 ? 1 : -1;
   ballYspeed = ballYspeed * plusOrminus;
 }
+                        //----------BALL MOVE--------------
 function ballMove() {
   ballX += ballXspeed;
   ballY += ballYspeed;
+
+                  //----------Ball Movement when hits player paddle
   if (
     ballX - ballRadius <= playerX + playerWidth &&
     ballY >= playerY &&
-    ballY <= playerY + playerHeight
-  ) {
+    ballY <= playerY + playerHeight &&
+    ballX - ballRadius >= playerX &&
+    ballXspeed < 0
+    ) {
     pushSound.play();
     let collidePoint = ballY - (playerY + playerHeight / 2);
     ballXspeed = -ballXspeed;
     ballYspeed = collidePoint * 0.35;
     ballXspeed += ballXspeed * 0.1;
   }
+
+            // ---------Ball Moevement when hits Computer paddle
   if (
     ballX + ballRadius >= compX - compWidth &&
     ballY >= compY &&
-    ballY <= compY + compHeight
+    ballY <= compY + compHeight &&
+    ballX + ballRadius <= compX &&
+    ballXspeed > 0
   ) {
     pushSound.play();
     let collidePointAi = ballY - (compY + compHeight / 2);
@@ -51,16 +60,23 @@ function ballMove() {
     ballXspeed += ballXspeed * 0.1;
   }
 
+            // -------What happens when ball hits LEFT border*/
   if (ballX - ballRadius <= 0) {
     compScore++;
     goalSound.play();
     resetBall();
   }
+
+            // ---------What happens when ball hits RIGHT border-------
+
   if (ballX + ballRadius >= canvasWidth) {
     playerScore++;
     goalSound.play();
     resetBall();
   }
+
+            // ---------What happens when ball hits Top or Down border-------
+
   if (ballY - ballRadius <= 0 || ballY + ballRadius >= canvasHeight) {
     ballYspeed = -ballYspeed;
   }
